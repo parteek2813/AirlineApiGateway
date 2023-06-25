e790475a90365550265442be3e21e41933c2ebbf
parteek kumar <digitalparteek@gmail.com>
2023-06-25 17:15:08 +0530

    proxy setup

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const PORT = 3005;
+
+app.use(morgan("combined"));
+app.use(
+  "/bookingservice/api/v1/home",
+  createProxyMiddleware({
+    target: "http://localhost:3002",
+    changeOrigin: true,
+  })
+);
 
 app.get("/home", (req, res) => {
   return res.json({ message: "OK" });
