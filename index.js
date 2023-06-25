d3184b32f6a1f75acfbdf4de735dea71f7d081a0
parteek kumar <digitalparteek@gmail.com>
2023-06-25 17:40:39 +0530

    Rate limiter setup

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const PORT = 3005;
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
+
 app.use(morgan("combined"));
+app.use(limiter);
 app.use(
-  "/bookingservice/api/v1/home",
+  "/bookingservice",
   createProxyMiddleware({
     target: "http://localhost:3002",
     changeOrigin: true,
